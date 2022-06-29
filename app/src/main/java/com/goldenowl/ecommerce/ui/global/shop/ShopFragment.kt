767cb4e84058a0fa852f3dbb5ebe8ff122c1d7f6
/paul-nguyen-goldenowl <paul.nguyen.goldenowl@gmail.com>
2022-06-29 17:34:48 +0700

    shop appbar add qr icon

--- app/src/main/java/com/goldenowl/ecommerce/ui/global/shop/ShopFragment.kt
+++ app/src/main/java/com/goldenowl/ecommerce/ui/global/shop/ShopFragment.kt
@@ -1,10 +1,8 @@
 package com.goldenowl.ecommerce.ui.global.shop
 
-import android.app.SearchManager
-import android.content.Context
 import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
-import androidx.appcompat.widget.SearchView
 import androidx.core.view.isEmpty
 import androidx.navigation.fragment.findNavController
 import com.goldenowl.ecommerce.R
@@ -14,7 +12,6 @@ import com.goldenowl.ecommerce.utils.Utils
 
 
 class ShopFragment : BaseHomeFragment<FragmentShopBinding>() {
-    private var searchView: SearchView? = null
 
     override fun getViewBinding(): FragmentShopBinding {
         return FragmentShopBinding.inflate(layoutInflater)
@@ -55,64 +52,28 @@ class ShopFragment : BaseHomeFragment<FragmentShopBinding>() {
         binding.topAppBar.appBarLayout.setBackgroundColor(
             Utils.getColor(requireContext(), R.color.background_color) ?: 0xF9F9F9
         )
+        binding.topAppBar.toolbar.setOnMenuItemClickListener {
+            Log.d(TAG, "setAppBarMenu: ${it.itemId}")
+            if (it.itemId == R.id.ic_search) {
+
+                binding.topAppBar.inputLayoutSearch.visibility = View.VISIBLE
+                Log.d(TAG, "setAppBarMenu: search clicked")
+            }
+            false
+        }
+
+        binding.topAppBar.inputLayoutSearch.setEndIconOnClickListener {
+            findNavController().navigate(R.id.qr_dest)
+        }
     }
 
 
     private fun setAppBarMenu() {
         val toolBar = binding.topAppBar.toolbar
-
         if (toolBar.menu.isEmpty()) {
             toolBar.apply {
-                inflateMenu(R.menu.menu_search)
+                inflateMenu(R.menu.menu_search_input)
             }
-            setSearchView()
-        }
-
-        /* todo: not work but refresh category array*/
-        if (searchView != null) {
-            searchView!!.setQuery("", false);
-        }
-    }
-
-    private fun setSearchView() {
-        val fullList = viewModel.categoryList.toList()
-
-        val menu = binding.topAppBar.toolbar.menu
-        val searchItem = menu.findItem(R.id.ic_search)
-        val searchManager = requireActivity().getSystemService(Context.SEARCH_SERVICE) as SearchManager
-
-        if (searchItem != null) {
-            searchView = searchItem.actionView as SearchView
-        }
-
-        val queryTextListener = object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(newText: String?): Boolean {
-                Log.i("onQueryTextChange", newText!!)
-//                filterCategory(fullList, newText)
-                //todo uiscope
-                return false
-            }
-
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                Log.i("onQueryTextSubmit", query!!)
-                /* close on submit */
-                searchView!!.apply {
-                    clearFocus()
-//                            visibility = View.GONE
-                }
-
-                return false
-            }
-        }
-        if (searchView != null) {
-            searchView!!.setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
-
-            searchView!!.setOnCloseListener {
-                Log.d(TAG, "setAppBarMenu: closed")
-                false
-            }
-            searchView!!.maxWidth = Integer.MAX_VALUE
-            searchView!!.setOnQueryTextListener(queryTextListener)
         }
     }
 
