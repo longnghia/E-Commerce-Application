884f307ade1e51c23ec3b805aebc547e6bfddb14
/paul-nguyen-goldenowl <paul.nguyen.goldenowl@gmail.com>
2022-07-01 14:45:53 +0700

    request permission add status text

--- app/src/main/java/com/goldenowl/ecommerce/ui/customview/MessageDialogFragment.kt
+++ app/src/main/java/com/goldenowl/ecommerce/ui/customview/MessageDialogFragment.kt
@@ -2,6 +2,7 @@ package com.goldenowl.ecommerce.ui.customview
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.goldenowl.ecommerce.R
@@ -10,8 +11,10 @@ import com.goldenowl.ecommerce.R
 class MessageDialogFragment : DialogFragment() {
     interface MessageDialogListener {
         fun onDialogPositiveClick(dialog: DialogFragment?)
+        fun onDialogNegativeClick(dialog: DialogFragment?)
     }
 
+    private var mType: DialogType? = null
     private var mTitle: String? = null
     private var mMessage: String? = null
     private var mListener: MessageDialogListener? = null
@@ -26,16 +29,36 @@ class MessageDialogFragment : DialogFragment() {
                 mListener!!.onDialogPositiveClick(this@MessageDialogFragment)
             }
         }
+        if(mType == DialogType.OPTION){
+            Log.d("type", "onCreateDialog: OPTION")
+            builder.setNegativeButton(getString(R.string.dont_allow)) { _, _ ->
+                if (mListener != null) {
+                    mListener!!.onDialogNegativeClick(this@MessageDialogFragment)
+                }
+            }
+        }
         return builder.create()
     }
 
     companion object {
-        fun newInstance(title: String?, message: String?, listener: MessageDialogListener?): MessageDialogFragment {
+        fun newInstance(
+            dialogType: DialogType,
+            title: String?,
+            message: String?,
+            listener: MessageDialogListener?
+        ): MessageDialogFragment {
             val fragment = MessageDialogFragment()
             fragment.mTitle = title
             fragment.mMessage = message
             fragment.mListener = listener
+            fragment.mType = dialogType
             return fragment
         }
+
+        enum class DialogType {
+            MESSAGE,
+            OPTION
+        }
     }
+
 }
--- app/src/main/java/com/goldenowl/ecommerce/ui/global/shop/QRFragment.kt
+++ app/src/main/java/com/goldenowl/ecommerce/ui/global/shop/QRFragment.kt
@@ -1,16 +1,16 @@
 package com.goldenowl.ecommerce.ui.global.shop
 
-import android.R.attr.x
-import android.R.attr.y
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Point
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.util.Log
+import android.view.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -18,17 +18,56 @@ import androidx.navigation.fragment.findNavController
 import com.goldenowl.ecommerce.R
 import com.goldenowl.ecommerce.databinding.FragmentQrBinding
 import com.goldenowl.ecommerce.ui.customview.MessageDialogFragment
-import com.goldenowl.ecommerce.utils.Utils
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 
 
-class QRFragment : Fragment(), ZXingScannerView.ResultHandler, MessageDialogFragment.MessageDialogListener {
+class QRFragment : Fragment(), ZXingScannerView.ResultHandler {
 
     private lateinit var mScannerView: ZXingScannerView
     private lateinit var binding: FragmentQrBinding
     private var mFlash = false
     private var mAutoFocus = false
+    private var permissionGranted = false
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                permissionGranted = true
+            } else {
+                findNavController().navigateUp()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        permissionGranted = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (!permissionGranted) {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        val fragment: DialogFragment = MessageDialogFragment.newInstance(
+            MessageDialogFragment.Companion.DialogType.OPTION,
+            getString(R.string.access_camera),
+            getString(R.string.camera_qr),
+            object : MessageDialogFragment.MessageDialogListener {
+                override fun onDialogPositiveClick(dialog: DialogFragment?) {
+                    requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                    dialog?.dismiss()
+                }
+
+                override fun onDialogNegativeClick(dialog: DialogFragment?) {
+                    findNavController().navigateUp()
+                }
+            }
+        )
+        fragment.show(activity!!.supportFragmentManager, "request_permission")
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentQrBinding.inflate(inflater, container, false)
@@ -44,36 +83,43 @@ class QRFragment : Fragment(), ZXingScannerView.ResultHandler, MessageDialogFrag
         return binding.root
     }
 
+
     private fun setViews() {
         mScannerView.setSquareViewFinder(true)
         binding.ivFlash.setOnClickListener {
+            if (!permissionGranted) {
+                requestPermission()
+                return@setOnClickListener
+            }
             mFlash = !mFlash
             it.isSelected = mFlash
             mScannerView.flash = mFlash
         }
 
-        val height = binding.root.height
-        val width = binding.root.width
-        val textView = TextView(context).apply {
-            text = "TEST123"
-            setTextColor(Utils.getColor(context, R.color.white) ?: 0xFFFFFF)
-        }
         val params = CoordinatorLayout.LayoutParams(
             CoordinatorLayout.LayoutParams.WRAP_CONTENT,
-            CoordinatorLayout.LayoutParams.WRAP_CONTENT
+            CoordinatorLayout.LayoutParams.WRAP_CONTENT,
         )
-        params.setMargins(x, width/2, x, y)
-        params.gravity = Gravity.CENTER
+        val display: Display = requireActivity().windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val width: Int = size.x
+        val height: Int = size.y
 
-        textView.layoutParams = params
-        binding.root.addView(textView)
+        Log.d(TAG, "setViews: width =$width")
+        params.setMargins(0, -width * 6 / 8, 0, 0)
+        params.gravity = Gravity.CENTER
+        binding.tvStatus.apply {
+            elevation = 50f
+            layoutParams = params
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        mScannerView.startCamera();          // Start camera on resume
-        mScannerView.setFlash(mFlash);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+        mScannerView.flash = mFlash;
         mScannerView.setAutoFocus(mAutoFocus);
     }
 
@@ -93,8 +139,6 @@ class QRFragment : Fragment(), ZXingScannerView.ResultHandler, MessageDialogFrag
         val TAG = "QRFragment"
         private const val FLASH_STATE = "FLASH_STATE"
         private const val AUTO_FOCUS_STATE = "AUTO_FOCUS_STATE"
-        private const val SELECTED_FORMATS = "SELECTED_FORMATS"
-        private const val CAMERA_ID = "CAMERA_ID"
     }
 
     override fun handleResult(rawResult: Result) {
@@ -102,13 +146,38 @@ class QRFragment : Fragment(), ZXingScannerView.ResultHandler, MessageDialogFrag
             val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
             val r = RingtoneManager.getRingtone(context, notification)
             r.play()
+            val res = rawResult.text
+            if (validQR(res)) {
+                val productId = res.split('=')[1]
+                showMessageDialog("Found $productId")
+            } else {
+                showMessageDialog("Invalid QR code")
+            }
         } catch (e: Exception) {
+            showMessageDialog(getString(R.string.scan_error))
         }
-        showMessageDialog("Contents = " + rawResult.text + ", Format = " + rawResult.barcodeFormat.toString())
+    }
+
+    private fun validQR(text: String?): Boolean {
+        if (text.isNullOrBlank()) return false
+        return text.indexOf("Product=") == 0
     }
 
     private fun showMessageDialog(message: String?) {
-        val fragment: DialogFragment = MessageDialogFragment.newInstance("Scan Results", message, this)
+        val fragment: DialogFragment = MessageDialogFragment.newInstance(
+            MessageDialogFragment.Companion.DialogType.MESSAGE,
+            "Scan Results",
+            message,
+            object : MessageDialogFragment.MessageDialogListener {
+                override fun onDialogPositiveClick(dialog: DialogFragment?) {
+                    mScannerView.resumeCameraPreview(this@QRFragment)
+                    findNavController().navigate(R.id.home_dest)
+                }
+
+                override fun onDialogNegativeClick(dialog: DialogFragment?) {
+                }
+            }
+        )
         fragment.show(activity!!.supportFragmentManager, "scan_results")
     }
 
@@ -119,16 +188,10 @@ class QRFragment : Fragment(), ZXingScannerView.ResultHandler, MessageDialogFrag
 
     private fun closeDialog(dialogName: String?) {
         val fragmentManager: FragmentManager = activity!!.supportFragmentManager
-        val fragment: DialogFragment = fragmentManager.findFragmentByTag(dialogName) as DialogFragment
-        if (fragment != null) {
-            fragment.dismiss()
+        val fragment = fragmentManager.findFragmentByTag(dialogName) ?: return
+        val diaglog = fragment as DialogFragment
+        if (diaglog != null) {
+            diaglog.dismiss()
         }
     }
-
-    override fun onDialogPositiveClick(dialog: DialogFragment?) {
-        // Resume the camera
-        mScannerView.resumeCameraPreview(this)
-        findNavController().navigate(R.id.home_dest)
-    }
-
 }
