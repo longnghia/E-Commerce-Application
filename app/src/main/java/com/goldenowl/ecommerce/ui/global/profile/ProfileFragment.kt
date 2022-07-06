66d9f6ae60cb1fceeb9b787a6434c94d78c6da80
/paul-nguyen-goldenowl <paul.nguyen.goldenowl@gmail.com>
2022-07-06 10:40:38 +0700

    update order in profile

--- app/src/main/java/com/goldenowl/ecommerce/ui/global/profile/ProfileFragment.kt
+++ app/src/main/java/com/goldenowl/ecommerce/ui/global/profile/ProfileFragment.kt
@@ -25,6 +25,7 @@ import com.goldenowl.ecommerce.ui.auth.LoginSignupActivity
 import com.goldenowl.ecommerce.ui.global.BaseFragment
 import com.goldenowl.ecommerce.ui.global.MainActivity
 import com.goldenowl.ecommerce.viewmodels.AuthViewModel
+import com.goldenowl.ecommerce.viewmodels.ShopViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -36,6 +37,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     private lateinit var auth: FirebaseAuth
     private lateinit var userManager: UserManager
     private val viewModel: AuthViewModel by activityViewModels()
+    private val shopViewModel: ShopViewModel by activityViewModels()
 
     override fun init() {
         userManager = UserManager.getInstance(requireContext())
@@ -79,6 +81,11 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     override fun setObservers() {
         viewModel.toastMessage.observe(viewLifecycleOwner) {
             Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        }
+
+        shopViewModel.allOrder.observe(viewLifecycleOwner) {
+            binding.tvActionOrders.text =
+                requireContext().resources.getQuantityString(R.plurals.num_order, it.size, it.size)
         }
     }
 
