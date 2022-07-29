397029a51c56ccd5f4f715dc2d293bd1f02d4eb4
/paul-nguyen-goldenowl <paul.nguyen.goldenowl@gmail.com>
2022-07-29 16:06:59 +0700

    fix app id for new package name

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ ls -l app/release/app-release.apk
 ls -l app/build/outputs/apk/release/app-release.apk
 ./gradlew bundleRelease
 firebase appdistribution:distribute app/release/app-release.apk  \
---app 1:174656673319:android:23f81df8e8d7885646e002  \
+--app 1:174656673319:android:8d3f5c6f67515b9c46e002  \
 --release-notes-file ./log.txt --groups go
