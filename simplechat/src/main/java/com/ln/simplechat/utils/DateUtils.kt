f8be68336f4d6425d314d13bee041bec47469f39
/paul-nguyen-goldenowl <paul.nguyen.goldenowl@gmail.com>
2022-08-24 17:23:37 +0700

    rename variable meaningfully

--- simplechat/src/main/java/com/ln/simplechat/utils/DateUtils.kt
+++ simplechat/src/main/java/com/ln/simplechat/utils/DateUtils.kt
@@ -75,13 +75,13 @@ object DateUtils {
     }
 
     fun isSameDate(date1: Long, date2: Long): Boolean {
-        val cal1 = Calendar.getInstance();
-        cal1.time = Date(date1)
-        val cal2 = Calendar.getInstance();
-        cal2.time = Date(date2)
-        return (cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA) &&
-                cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
-                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR))
+        val calender1 = Calendar.getInstance()
+        calender1.time = Date(date1)
+        val calender2 = Calendar.getInstance()
+        calender2.time = Date(date2)
+        return (calender1.get(Calendar.ERA) == calender2.get(Calendar.ERA) &&
+                calender1.get(Calendar.YEAR) == calender2.get(Calendar.YEAR) &&
+                calender1.get(Calendar.DAY_OF_YEAR) == calender2.get(Calendar.DAY_OF_YEAR))
     }
 
     fun dateDiffer(d: Long): Int {
